c9a810162d1410ae31011558ecd014b187cbd243
angelsc75 <asc7es@gmail.com>
2025-05-26 15:05:24 +0200

    Update llm.py
    llm, conexión y su uso también para hacer los embedings

--- llm.py
+++ llm.py
@@ -1,6 +1,20 @@
 import streamlit as st
+from langchain_openai import ChatOpenAI
+from langchain_openai import OpenAIEmbeddings
 
+# Create the LLM
 # Create the LLM
 
 
+llm = ChatOpenAI(
+    openai_api_key=st.secrets["OPENAI_API_KEY"],
+    model=st.secrets["OPENAI_MODEL"],
+)
+
 # Create the Embedding model
+# Create the Embedding model
+
+
+embeddings = OpenAIEmbeddings(
+    openai_api_key=st.secrets["OPENAI_API_KEY"]
+)
